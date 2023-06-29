419153b98a5f807db2991d9150e2b526e1b1026f
EmadHanif <emadhanif200@gmail.com>
2023-06-29 13:12:12 +0500

    Code Restructuring | Jsonnet Integration

--- Examples/EdgeChainApplication.java
+++ Examples/EdgeChainApplication.java
@@ -1,6 +1,7 @@
 package com.edgechain;
 
 import com.edgechain.lib.chains.PineconeRetrieval;
+import com.edgechain.lib.chains.RedisRetrieval;
 import com.edgechain.lib.chains.retrieval.Retrieval;
 import com.edgechain.lib.constants.WebConstants;
 import com.edgechain.lib.configuration.EdgeChainAutoConfiguration;
@@ -17,6 +18,7 @@ import com.edgechain.lib.feign.OpenAiService;
 import com.edgechain.lib.feign.OpenAiStreamService;
 import com.edgechain.lib.feign.WikiService;
 import com.edgechain.lib.feign.index.PineconeService;
+import com.edgechain.lib.feign.index.RedisService;
 import com.edgechain.lib.jsonnet.JsonnetArgs;
 import com.edgechain.lib.jsonnet.JsonnetLoader;
 import com.edgechain.lib.jsonnet.enums.DataType;
@@ -29,6 +31,7 @@ import com.edgechain.lib.reader.impl.PdfReader;
 import com.edgechain.lib.request.OpenAiChatRequest;
 import com.edgechain.lib.request.OpenAiEmbeddingsRequest;
 import com.edgechain.lib.request.PineconeRequest;
+import com.edgechain.lib.request.RedisRequest;
 import com.edgechain.lib.response.ArkResponse;
 import com.edgechain.lib.response.ArkEmitter;
 import com.edgechain.lib.rxjava.response.ChainResponse;
@@ -70,7 +73,7 @@ public class EdgeChainApplication implements CommandLineRunner {
 
     System.setProperty("server.port", port);
     System.setProperty("server.url", String.format("%s:%s%s", host, port, SERVICE_CONTEXT_PATH));
-    
+
     System.setProperty("OPENAI_AUTH_KEY", "");
 
     System.setProperty("PINECONE_AUTH_KEY", "");
@@ -238,11 +241,15 @@ public class EdgeChainApplication implements CommandLineRunner {
   public class PineconeOpenAiController {
 
     @Autowired private PdfReader pdfReader;
-    @Autowired private EmbeddingService embeddingService_;
-    @Autowired private PineconeService pineconeService_;
 
     @PostMapping(value = "/upsert", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void upsertByChunk(@RequestParam(value = "file") MultipartFile file) throws IOException {
+
+      JsonnetLoader loader = new FileJsonnetLoader("R:\\pinecone-query.jsonnet");
+      Schema schema = loader.loadOrReload(new HashMap<>(), Schema.class);
+
+      EmbeddingService embeddingService = new ServiceMapper().map(schema, "embeddingService", EmbeddingService.class);
+      PineconeService pineconeService = new ServiceMapper().map(schema, "pineconeService", PineconeService.class);
 
       String[] arr = pdfReader.readByChunkSize(file.getInputStream(), 512);
 
@@ -261,7 +268,7 @@ public class EdgeChainApplication implements CommandLineRunner {
 
       Retrieval retrieval =
               new PineconeRetrieval(
-                      pineconeEndpoint, embeddingEndpoint, embeddingService_, pineconeService_);
+                      pineconeEndpoint, embeddingEndpoint, embeddingService, pineconeService);
 
       IntStream.range(0, arr.length).parallel().forEach(i -> retrieval.upsert(arr[i]));
     }
@@ -507,6 +514,270 @@ public class EdgeChainApplication implements CommandLineRunner {
                 .getArkResponse();
     }
   }
+
+  @RestController
+  @RequestMapping("/v1/redis/openai")
+  public class RedisOpenAiController {
+
+    @Autowired private PdfReader pdfReader;
+
+    @PostMapping(value = "/upsert", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void upsertByChunk(@RequestParam(value = "file") MultipartFile file) throws IOException {
+
+      JsonnetLoader loader = new FileJsonnetLoader("R:\\redis-query.jsonnet");
+      Schema schema = loader.loadOrReload(new HashMap<>(), Schema.class);
+
+      EmbeddingService embeddingService =
+              new ServiceMapper().map(schema, "embeddingService", EmbeddingService.class);
+      RedisService redisService =
+              new ServiceMapper().map(schema, "redisService", RedisService.class);
+
+      String[] arr = pdfReader.readByChunkSize(file.getInputStream(), 512);
+
+      Endpoint embeddingEndpoint =
+              new Endpoint(
+                      OPENAI_EMBEDDINGS_API,
+                      OPENAI_AUTH_KEY,
+                      "text-embedding-ada-002",
+                      new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+
+      Retrieval retrieval = new RedisRetrieval(embeddingEndpoint, embeddingService, redisService);
+
+      IntStream.range(0, arr.length).parallel().forEach(i -> retrieval.upsert(arr[i]));
+    }
+
+
+    @GetMapping(
+            value = "/query",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_EVENT_STREAM_VALUE})
+    public Object query(
+            @RequestParam Integer topK, @RequestParam Boolean stream, @RequestParam String query) {
+
+      HashMap<String, JsonnetArgs> parameters = new HashMap<>();
+      parameters.put("keepMaxTokens", new JsonnetArgs(DataType.BOOLEAN, "true"));
+      parameters.put("maxTokens", new JsonnetArgs(DataType.INTEGER, "4096"));
+
+      JsonnetLoader loader = new FileJsonnetLoader("R:\\redis-query.jsonnet");
+      Schema schema = loader.loadOrReload(parameters, Schema.class);
+
+      Endpoint chatEndpoint =
+              new Endpoint(
+                      OPENAI_CHAT_COMPLETION_API,
+                      OPENAI_AUTH_KEY,
+                      "gpt-3.5-turbo",
+                      "user",
+                      0.7,
+                      new ExponentialDelay(3, 5, 2, TimeUnit.SECONDS));
+
+      Endpoint embeddingEndpoint =
+              new Endpoint(
+                      OPENAI_EMBEDDINGS_API,
+                      OPENAI_AUTH_KEY,
+                      "text-embedding-ada-002",
+                      new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+
+
+      RedisService redisService =
+              new ServiceMapper().map(schema, "redisService", RedisService.class);
+      EmbeddingService embeddingService =
+              new ServiceMapper().map(schema, "embeddingService", EmbeddingService.class);
+      OpenAiService openAiService =
+              new ServiceMapper().map(schema, "openAiService", OpenAiService.class);
+
+      if (stream) {
+
+        System.out.println("Using Stream");
+
+        String[] redisQueries =
+                create(
+                        embeddingService
+                                .openAi(new OpenAiEmbeddingsRequest(embeddingEndpoint, query))
+                                .getResponse())
+                        .transform(
+                                embeddingOutput ->
+                                        redisService
+                                                .query(new RedisRequest(embeddingOutput, topK))
+                                                .getResponse())
+                        .transform(redisOutput -> redisOutput.split("\n"))
+                        .getWithOutRetry();
+
+        AtomInteger currentTopK = AtomInteger.of(0);
+
+        return new EdgeChain<>(
+                Observable.create(
+                        emitter -> {
+                          try {
+                            String input = redisQueries[currentTopK.getAndIncrement()];
+
+                            parameters.put("keepContext", new JsonnetArgs(DataType.BOOLEAN, "true"));
+                            parameters.put("context", new JsonnetArgs(DataType.STRING, input));
+
+                            Schema schema_ = loader.loadOrReload(parameters, Schema.class);
+
+                            emitter.onNext(
+                                    openAiService.chatCompletion(
+                                            new OpenAiChatRequest(chatEndpoint, schema_.getPrompt())));
+                            emitter.onComplete();
+
+                          } catch (final Exception e) {
+                            emitter.onError(e);
+                          }
+                        }))
+                .doWhileLoop(() -> currentTopK.get() == ((int) topK))
+                .getArkEmitter();
+      } else {
+        // Creation of Chains
+        return create(
+                embeddingService
+                        .openAi(new OpenAiEmbeddingsRequest(embeddingEndpoint, query))
+                        .getResponse())
+                .transform(
+                        embeddingOutput ->
+                                redisService
+                                        .query(new RedisRequest(embeddingOutput, topK))
+                                        .getResponse())
+                .transform(
+                        redisOutput -> {
+                          List<ChainResponse> output = new ArrayList<>();
+
+                          StringTokenizer tokenizer = new StringTokenizer(redisOutput, "\n");
+                          while (tokenizer.hasMoreTokens()) {
+
+                            String response = tokenizer.nextToken();
+                            // Use jsonnet loader
+                            parameters.put("keepContext", new JsonnetArgs(DataType.BOOLEAN, "true"));
+                            parameters.put("context", new JsonnetArgs(DataType.STRING, response));
+
+                            Schema schema_ = loader.loadOrReload(parameters, Schema.class);
+                            output.add(
+                                    openAiService.chatCompletion(
+                                            new OpenAiChatRequest(chatEndpoint, schema_.getPrompt())));
+                          }
+
+                          return output;
+                        })
+                .getArkResponse();
+      }
+    }
+
+    /**
+     *
+     * @param contextId
+     * @param stream
+     * @param query
+     * @return ArkResponse
+     */
+    @GetMapping(
+            value = "/query/context",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_EVENT_STREAM_VALUE})
+    public Object queryWithChatHistory(
+            @RequestParam String contextId, @RequestParam Boolean stream, @RequestParam String query) {
+
+      HashMap<String, JsonnetArgs> parameters = new HashMap<>();
+
+      parameters.put("keepMaxTokens", new JsonnetArgs(DataType.BOOLEAN, "true"));
+      parameters.put("maxTokens", new JsonnetArgs(DataType.INTEGER, "4096"));
+      parameters.put("query", new JsonnetArgs(DataType.STRING, query));
+      parameters.put("keepHistory", new JsonnetArgs(DataType.BOOLEAN, "false"));
+
+      JsonnetLoader loader = new FileJsonnetLoader("R:\\redis-chat.jsonnet");
+      ChatSchema schema = loader.loadOrReload(parameters, ChatSchema.class);
+
+      Endpoint embeddingEndpoint =
+              new Endpoint(
+                      OPENAI_EMBEDDINGS_API,
+                      OPENAI_AUTH_KEY,
+                      "text-embedding-ada-002",
+                      new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+
+      Endpoint chatEndpoint =
+              new Endpoint(
+                      OPENAI_CHAT_COMPLETION_API,
+                      OPENAI_AUTH_KEY,
+                      "gpt-3.5-turbo",
+                      "user",
+                      0.7,
+                      stream,
+                      new ExponentialDelay(3, 3, 2, TimeUnit.SECONDS));
+
+      RedisService redisService =
+              new ServiceMapper().map(schema, "redisService", RedisService.class);
+      EmbeddingService embeddingService =
+              new ServiceMapper().map(schema, "embeddingService", EmbeddingService.class);
+      OpenAiService openAiService =
+              new ServiceMapper().map(schema, "openAiService", OpenAiService.class);
+      OpenAiStreamService openAiStreamService =
+              new ServiceMapper().map(schema, "openAiStreamService", OpenAiStreamService.class);
+      HistoryContextService contextService =
+              new ServiceMapper().map(schema, "historyContextService", HistoryContextService.class);
+
+      // Creating Chains
+      EdgeChain<Tuple2<String,String>> edgeChain =
+              create(
+                      embeddingService
+                              .openAi(new OpenAiEmbeddingsRequest(embeddingEndpoint, query))
+                              .getResponse())
+                      .transform(
+                              embeddingOutput ->
+                                      redisService
+                                              .query(
+                                                      new RedisRequest(embeddingOutput, schema.getTopK()))
+                                              .getResponse())
+                      .transform(
+                              redisOutput -> {
+                                System.out.printf("Query %s-%s", schema.getTopK(), redisOutput);
+
+                                // Query, Preset, RedisOutput, ChatHistory
+                                String chatHistory = contextService.get(contextId).getWithRetry().getResponse();
+
+                                parameters.put("keepHistory", new JsonnetArgs(DataType.BOOLEAN, "true"));
+                                parameters.put("history", new JsonnetArgs(DataType.STRING, chatHistory));
+
+                                parameters.put("keepContext", new JsonnetArgs(DataType.BOOLEAN, "true"));
+                                parameters.put("context", new JsonnetArgs(DataType.STRING, redisOutput));
+
+                                ChatSchema schema_ = loader.loadOrReload(parameters, ChatSchema.class);
+
+                                // ChatHistory, Prompt
+                                return new Tuple2<>(chatHistory,schema_.getPrompt());
+                              });
+
+      if (chatEndpoint.getStream()) {
+
+        Tuple2<String,String> tuple2 = edgeChain.getWithOutRetry();
+
+        System.out.println("\nPrompt: \n"+tuple2._2);
+
+        StringBuilder openAiResponseBuilder = new StringBuilder();
+        return new ArkEmitter<>(
+                openAiStreamService.chatCompletion(new OpenAiChatRequest(chatEndpoint,tuple2._2)).doOnNext(
+                        v -> {
+                          if (v.getResponse().equals(WebConstants.CHAT_STREAM_EVENT_COMPLETION_MESSAGE)) {
+                            String redisHistory = query + openAiResponseBuilder.toString().replaceAll("[\t\n\r]+", " ") + tuple2._1;
+                            contextService.put(contextId, redisHistory).getWithRetry();
+                          } else {
+                            openAiResponseBuilder.append(v.getResponse());
+                          }
+                        }));
+
+      } else
+        return edgeChain
+                .transform(
+                        tuple2 -> {
+                          String openAiResponse =
+                                  openAiService
+                                          .chatCompletion(new OpenAiChatRequest(chatEndpoint, tuple2._2))
+                                          .getResponse();
+
+                          contextService.put(contextId, query + openAiResponse + tuple2._1).getWithRetry();
+
+                          return openAiResponse;
+                        })
+                .getArkResponse();
+    }
+
+  }
+  
 
   /************ EXAMPLE APIs **********************/
 }
